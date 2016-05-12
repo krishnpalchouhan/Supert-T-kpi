c634cd3d32d13291d1c43fae7e9dc70c3a08026b
Krishnpalchouhan <krishnpalchouhan@gmail.com>
2016-05-13 05:15:44 +0530

    copy from working

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,21 @@
-/////////////////////////////////////ios code//////////////////
 /*
-var token;
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -18,71 +33,19 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        
-        var push = PushNotification.init({
-                             "android": {
-                             "senderID": "781247844618"
-                             },
-                             "ios": {
-                             "alert": "true",
-                             "badge": "true",
-                             "sound": "true"
-                             }
-                         });
-        
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        setTimeout(checkToken,500);
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
         console.log('Received Event: ' + id);
     }
 };
 
-
-function checkToken() {
-    cordova.exec(function(message) {
-                 token = message.token;
-                 if (token.length == 0) {
-                    setTimeout(checkToken,120);
-                    return;
-                 }
-                 window.localStorage.setItem("player_id",token);
-                 },null,'GetToken','getToken',null);
-}
-
-app.initialize();*/
-/////////////////////////////////////ios code//////////////////
-
-//window.location = "dashbord.html";		
-		// Add to index.js or the first page that loads with your app.
-			// For Intel XDK and please add this to your app.js.
-
-			document.addEventListener('deviceready', function () {
-	
-			// Enable to debug issues.
-			// window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-			  var notificationOpenedCallback = function(jsonData) {
-				console.log('didReceiveRemoteNotificationCallBack: ' + JSON.stringify(jsonData));
-			
-			  };
-
-			  window.plugins.OneSignal.init("54a45a1c-c6cc-491c-b963-aa4dcc8f6abd",
-											 {googleProjectNumber: "977668981273"},
-											 notificationOpenedCallback);
-			  	
-			  // Show an alert box if a notification comes in when the user is in your app.
-				window.plugins.OneSignal.enableInAppAlertNotification(true);
-				window.plugins.OneSignal.getIds(function(ids) {
-				alert(ids.userId);
-				window.localStorage.setItem("player_id",ids.userId );
-				//window.location = "dashbord.html";
-				//document.getElementById("OneSignalPushToken").innerHTML = "PushToken: " + ids.pushToken;
-				 // console.log('getIds: ' + JSON.stringify(ids));
-				});
-			  window.plugins.OneSignal.getIds();
-                while(ids.userId==""){
-                    window.location = "index.html";
-                }
-			
-			}, false); 
+app.initialize();
